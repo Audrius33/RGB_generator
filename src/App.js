301aa8964473d0b224d9b4d9476759b2fdd84d08
audri <audriusambrazevicius@gmail.com>
2021-09-23 07:53:20 +0300

    readme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef, useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [buttonColor, setButtonColor] = useState("")
+
+    const rRef = useRef()
+    const gRef = useRef()
+    const bRef = useRef()
+
+
+    function RGBtoHex() {
+        const rValue = rRef.current.value
+        const gValue = gRef.current.value
+        const bValue = bRef.current.value
+        const r = parseInt(rValue).toString(16)
+        const g = parseInt(gValue).toString(16)
+        const b = parseInt(bValue).toString(16)
+
+        const result = `#${r.length === 1 ? 0 + r : r}${g.length === 1 ? 0 + g : g}${b.length === 1 ? 0 + b : b}`
+        setButtonColor(result)
+    }
+
+    return (
+
+        <div>
+            <input ref={rRef} type="number" placeholder="R"/>
+            <input ref={gRef} type="number" placeholder="G"/>
+            <input ref={bRef} type="number" placeholder="B"/>
+            <button style={{backgroundColor: buttonColor}} onClick={RGBtoHex}>change color</button>
+        </div>
+    );
 }
 
 export default App;
